72a2e6602fa84273cb39d3e294f74fd440c087f6
andrewjlroth <andrewjlroth@localhost>
2011-02-01 17:13:26 -0800

    Refactored run validation to run all models.

--- generate_validation.py
+++ generate_validation.py
@@ -4,24 +4,26 @@ import os
 from joint_snv_mix.file_formats.jsm import JointSnvMixReader
 from joint_snv_mix.classification.classification import run_classifier
 from argparse import Namespace
-from bisect import bisect_right
 import bisect
 
 excluded_chrom = ['MT', 'Y']
 
 def main( validation_dir ):    
     varscan_predictions = load_varscan_predictions( validation_dir )
+    write_predictions_to_tsv( varscan_predictions, validation_dir, "varscan" )
     
-    jsm_files, ism_files = run_jsm( validation_dir )
+    models = {
+              'indep_bin' : run_indep_bin,
+              'indep_bb' : run_indep_bb,
+              'joint_bin' : run_joint_bin,
+              'joint_bb' : run_joint_bb
+              }
     
-    joint_predictions = load_jsm_predictions( jsm_files, varscan_predictions )
-    
-    indep_predictions = load_jsm_predictions( ism_files, varscan_predictions )
-    
-    compare_positions( varscan_predictions, validation_dir, "varscan" )
-    compare_positions( indep_predictions, validation_dir, "indep" )
-    compare_positions( joint_predictions, validation_dir, "joint" )
-    
+    for model_name, run_func in models.iteritems():            
+        files = run_model( validation_dir, model_name, run_func )
+        predictions = load_jsm_predictions( files, varscan_predictions )
+        write_predictions_to_tsv( predictions, validation_dir, model_name )
+            
 def load_varscan_predictions( validation_dir ):
     glob_str = os.path.join( validation_dir, "*.hc" )
     
@@ -62,56 +64,83 @@ def load_predictions_from_varscan_file( file_name ):
     
     return positions
 
-def run_jsm( validation_dir ):
+def run_model( validation_dir, model_name, run_model_func ):
     glob_str = os.path.join( validation_dir, "*.jcnt" )
     
     jcnt_file_list = glob.glob( glob_str )
     
-    jsm_files = {}
-    
-    ism_files = {}
+    model_files = {}
     
     for jcnt_file_name in jcnt_file_list:
         base_name = os.path.basename( jcnt_file_name )
         
         case = base_name.split( '.' )[0]
-        
-        if case not in jsm_files.keys():
-            jsm_files[case] = {}
-            ism_files[case] = {}
-            
         tech = base_name.split( '.' )[1]
         
-        ism_file_name = case + "." + tech + ".ism"
-        ism_file_name = os.path.join( validation_dir, ism_file_name )
+        if case not in model_files.keys():
+            model_files[case] = {}    
         
-        ism_files[case][tech] = ism_file_name
+        #===============================================================================================================
+        # Indep Bin
+        #===============================================================================================================
+        model_file_name = case + "." + tech + "." + model_name + ".jsm" 
         
-        if not os.path.exists( ism_file_name ):
+        model_file_name = os.path.join( validation_dir, model_file_name )
+        
+        model_files[case][tech] = model_file_name
+        
+        if not os.path.exists( model_file_name ):
             try:
-                run_indep_bin( jcnt_file_name, ism_file_name )
+                run_model_func( jcnt_file_name, model_file_name )
             except:
                 pass 
-        
-        jsm_file_name = case + "." + tech + ".jsm"
-        jsm_file_name = os.path.join( validation_dir, jsm_file_name )
-               
-        jsm_files[case][tech] = jsm_file_name
-        
-        if not os.path.exists( jsm_file_name ):
-            try:
-                run_joint_bb( jcnt_file_name, jsm_file_name )
-            except:
-                pass       
-    
-    return jsm_files, ism_files
 
-def run_joint_bb( jcnt_file_name, jsm_file_name ):
+def run_indep_bin( jcnt_file_name, jsm_file_name ):
+    args = {}
+    
+    args['model'] = "independent"
+    args['density'] = "binomial"
+    args['priors_file'] = '/home/andrew/workspace/joint_snv_mix/config/indep_bin.priors.cfg'
+    
+    args['jcnt_file_name'] = jcnt_file_name
+    args['jsm_file_name'] = jsm_file_name
+    
+    args['max_iters'] = int( 1e3 ) 
+    args['convergence_threshold'] = float( 1e-6 )
+    
+    args['train'] = True
+    args['subsample_size'] = int( 1e6 )
+    
+    args = Namespace( **args )
+    
+    run_classifier( args )
+    
+def run_indep_bb( jcnt_file_name, jsm_file_name ):
+    args = {}
+    
+    args['model'] = "independent"
+    args['density'] = "beta_binomial"
+    args['priors_file'] = '/home/andrew/workspace/joint_snv_mix/config/indep_bb.priors.cfg'
+    
+    args['jcnt_file_name'] = jcnt_file_name
+    args['jsm_file_name'] = jsm_file_name
+    
+    args['max_iters'] = int( 1e3 ) 
+    args['convergence_threshold'] = float( 1e-6 )
+    
+    args['train'] = True
+    args['subsample_size'] = int( 1e6 )
+    
+    args = Namespace( **args )
+    
+    run_classifier( args )
+    
+def run_joint_bin( jcnt_file_name, jsm_file_name ):
     args = {}
     
     args['model'] = "joint"
-    args['density'] = "beta_binomial"
-    args['priors_file'] = '/home/andrew/workspace/joint_snv_mix/config/joint_bb_em.priors.cfg'
+    args['density'] = "binomial"
+    args['priors_file'] = '/home/andrew/workspace/joint_snv_mix/config/joint_bin.priors.cfg'
     
     args['jcnt_file_name'] = jcnt_file_name
     args['jsm_file_name'] = jsm_file_name
@@ -126,15 +155,15 @@ def run_joint_bb( jcnt_file_name, jsm_file_name ):
     
     run_classifier( args )
 
-def run_indep_bin( jcnt_file_name, ism_file_name ):
+def run_joint_bb( jcnt_file_name, jsm_file_name ):
     args = {}
     
-    args['model'] = "independent"
-    args['density'] = "binomial"
-    args['priors_file'] = '/home/andrew/workspace/joint_snv_mix/config/indep_bin_em.priors.cfg'
+    args['model'] = "joint"
+    args['density'] = "beta_binomial"
+    args['priors_file'] = '/home/andrew/workspace/joint_snv_mix/config/joint_bb.priors.cfg'
     
     args['jcnt_file_name'] = jcnt_file_name
-    args['jsm_file_name'] = ism_file_name
+    args['jsm_file_name'] = jsm_file_name
     
     args['max_iters'] = int( 1e3 ) 
     args['convergence_threshold'] = float( 1e-6 )
@@ -157,21 +186,11 @@ def load_jsm_predictions( jsm_files, varscan_predictions ):
             
             n = len( varscan_predictions[case][tech] )
             
-            predictions = load_predictions_from_jsm_file( jsm_files[case][tech], n )
-            
-#            predictions = predictions[:n]
+            predictions = load_somatics( jsm_files[case][tech], n )
             
             jsm_predictions[case][tech] = predictions
     
     return jsm_predictions
-    
-
-def load_predictions_from_jsm_file( jsm_file_name, n ):
-    position_score = load_somatics( jsm_file_name, n )
-    
-#    position_score = sort_position_score( position_score )    
-    
-    return position_score
 
 def load_somatics( jsm_file_name, n ):
     reader = JointSnvMixReader( jsm_file_name )
@@ -184,7 +203,7 @@ def load_somatics( jsm_file_name, n ):
     for chr_name in sorted( chr_list ):
         if chr_name in excluded_chrom:
             continue
-#        
+                
         chr_rows = reader.get_rows( chr_name )
         
         for row in chr_rows:
@@ -199,21 +218,13 @@ def load_somatics( jsm_file_name, n ):
                 
                 if len( scores ) >= n:
                     scores.pop( 0 )
-                    position_score.pop( 0 )
-                
-                print position, insert_position, position_score[0], position_score[-1]
-            
-    
+                    position_score.pop( 0 )                        
+                        
     reader.close()
         
     return position_score
 
-def sort_position_score( position_score ):
-    position_score = sorted( position_score, key=lambda x: x[2], reverse=True )
-    
-    return position_score
-
-def compare_positions( predictions, dir, method_name ):
+def write_predictions_to_tsv( predictions, dir, method_name ):
     for case in predictions.keys():
         for tech in predictions[case].keys():
             file_name = case + "." + tech + "." + method_name + ".tsv"
@@ -223,15 +234,6 @@ def compare_positions( predictions, dir, method_name ):
             writer = csv.writer( open( file_name, 'w' ), delimiter='\t' )
             
             writer.writerows( predictions[case][tech] )
-
-
-def get_position_set( predictions ):
-    positions = set()
-    
-    for prediction in predictions:
-        positions.add( tuple( prediction[:2] ) )
-    
-    return positions
         
     
 if __name__ == "__main__":
