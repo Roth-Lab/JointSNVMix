53d79f8b79b7d055deed60407a9ec292aacf9180
andrewjlroth <andrewjlroth@localhost>
2011-02-24 11:50:59 -0800

    Update to version 0.5.1 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ ext_modules = [Extension("joint_snv_mix.file_formats.pileup", ["joint_snv_mix/fi
 
 setup(
       name='JointSNVMix',
-      version='0.5.0',
+      version='0.5.1',
       description='Python SNV calling utility.',
       
       author='Andrew Roth',
