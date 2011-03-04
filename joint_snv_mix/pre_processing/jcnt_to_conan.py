0f4ac4afc5de02e44330e9656254cc3bddaeebca
andrewjlroth <andrewjlroth@localhost>
2011-03-03 16:57:02 -0800

    Changed jcnt_to_conan to only tables when chromosome changes.

--- joint_snv_mix/pre_processing/jcnt_to_conan.py
+++ joint_snv_mix/pre_processing/jcnt_to_conan.py
@@ -11,19 +11,18 @@ from joint_snv_mix.file_formats.jcnt import JointCountsReader
 from joint_snv_mix.file_formats.cncnt import ConanCountsFile
 from argparse import Namespace
 
-
-def jcnt_to_cncnt( args ):
-    reader = JointCountsReader( args.jcnt_file_name )
+def jcnt_to_cncnt(args):
+    reader = JointCountsReader(args.jcnt_file_name)
     
     chr_list = reader.get_chr_list()
     
-    cncnt_file = ConanCountsFile( args.cncnt_file_name, 'w' )
+    cncnt_file = ConanCountsFile(args.cncnt_file_name, 'w')
     
-    segment_reader = csv.reader( open( args.segment_file_name ), delimiter='\t' )
+    segment_reader = csv.reader(open(args.segment_file_name), delimiter='\t')
+
+    old_chr_name = None
 
     for row in segment_reader:
-        print row
-        
         chr_name = row[0]
         
         if chr_name == '23':
@@ -31,8 +30,8 @@ def jcnt_to_cncnt( args ):
         if chr_name == '24':
             chr_name = 'Y'
         
-        start = int( row[1] )
-        stop = int( row[2] )
+        start = int(row[1])
+        stop = int(row[2])
         cn_status = row[3]
         
         if cn_status == '7':
@@ -47,18 +46,23 @@ def jcnt_to_cncnt( args ):
             cn_status = '6'
         
         if chr_name not in chr_list:
+            print chr_name
             continue
         
-        rows = reader.get_rows( chr_name )
+        # Load new table if chromosome has changed.
+        if chr_name != old_chr_name:
+            rows = reader.get_rows(chr_name)
         
-        segment_indices = np.logical_and( rows['position'] >= start, rows['position'] <= stop )
+        old_chr_name = chr_name        
+        
+        segment_indices = np.logical_and(rows['position'] >= start, rows['position'] <= stop)
         
         segment_rows = rows[segment_indices]
         
-        if len( segment_rows ) == 0:
+        if len(segment_rows) == 0:
             continue
         
-        cncnt_file.add_rows( cn_status, chr_name, segment_rows )
+        cncnt_file.add_rows(cn_status, chr_name, segment_rows)
     
     reader.close()
     cncnt_file.close()
@@ -73,4 +77,4 @@ if __name__ == "__main__":
     args.segment_file_name = sys.argv[3]
     
     
-    jcnt_to_cncnt( args )
+    jcnt_to_cncnt(args)
