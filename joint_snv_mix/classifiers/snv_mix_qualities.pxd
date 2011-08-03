5115b48c9e79215d465992190a443e59d7c29998
aroth <aroth@localhost>
2011-08-03 14:35:03 -0700

    Refactored SNVMix2 code.

--- joint_snv_mix/classifiers/snv_mix_qualities.pxd
+++ joint_snv_mix/classifiers/snv_mix_qualities.pxd
@@ -1,6 +1,6 @@
 #cython: cdivision=True
-
-from libc.math cimport exp, log
+from libc.stdlib cimport free
+from libc.math cimport log
 
 from joint_snv_mix.counters.joint_binary_quality_counter cimport JointBinaryQualityCounterRow, \
                                                                  JointBinaryQualityCounterIterator, \
@@ -8,6 +8,9 @@ from joint_snv_mix.counters.joint_binary_quality_counter cimport JointBinaryQual
                                                                  base_map_qualities_struct
                                                                  
 from joint_snv_mix.classifiers.classifier cimport Classifier, ClassifierRefIterator, ClassifierRow
+
+from joint_snv_mix.classifiers.shared cimport snv_mix_2_log_likelihood, get_mixture_posterior, \
+                                              combine_independent_probs, get_phred_to_prob_qual_map
 
 from joint_snv_mix.utils.normalise cimport log_space_normalise_row
 
@@ -18,27 +21,9 @@ cdef class SnvMix2Classifier(Classifier):
     pass
              
 cdef class SnvMix2ClassifierRefIterator(ClassifierRefIterator):              
-    cdef double _qual_map[256]
+    cdef double * _qual_map
        
     cdef double _mu_N[NUM_GENOTYPES]
     cdef double _mu_T[NUM_GENOTYPES]
     cdef double _log_pi_N[NUM_GENOTYPES]
     cdef double _log_pi_T[NUM_GENOTYPES]
-
-    cdef void _get_probs(
-                         self,
-                         double probs[NUM_GENOTYPES],
-                         base_map_qualities_struct data,
-                         double mu[NUM_GENOTYPES],
-                         double log_pi[NUM_GENOTYPES]
-                         )
-    
-    cdef void _combine_probs(
-                             self,
-                             double joint_probs[NUM_JOINT_GENOTYPES],
-                             double normal_probs[NUM_GENOTYPES],
-                             double tumour_probs[NUM_GENOTYPES]
-                             )
-    
-    cdef double _compute_single_base_log_prob(self, double q, double r, double m)
-    cdef _init_qual_map(self)
--- joint_snv_mix/classifiers/snv_mix_qualities.pyx
+++ joint_snv_mix/classifiers/snv_mix_qualities.pyx
@@ -18,8 +18,18 @@ cdef class SnvMix2ClassifierRefIterator(ClassifierRefIterator):
     def __init__(self, char * ref, JointBinaryQualityCounterIterator iter, **kwargs):
         ClassifierRefIterator.__init__(self, ref, iter, **kwargs)
         
-        self._init_qual_map()
+        print "S"
         
+        self._qual_map = get_phred_to_prob_qual_map(256)
+        
+        self._init_params(**kwargs)
+        
+        print self._qual_map[0]
+            
+    def __dealloc__(self):
+        free(self._qual_map)
+    
+    def _init_params(self, **kwargs):
         mu_N = kwargs.get('mu_N', (0.99, 0.5, 0.01))
         mu_T = kwargs.get('mu_T', (0.99, 0.5, 0.01))
         pi_N = kwargs.get('pi_N', (0.99, 0.009, 0.001))
@@ -31,97 +41,36 @@ cdef class SnvMix2ClassifierRefIterator(ClassifierRefIterator):
             
             self._log_pi_N[i] = log(pi_N[i])            
             self._log_pi_T[i] = log(pi_T[i])
-    
-    cdef _init_qual_map(self):
-        cdef int qual
-        cdef double base, exp
-        
-        for qual in range(256):
-            exp = -1 * (< double > qual) / 10
-            base = 10
-            
-            self._qual_map[qual] = 1 - pow(base, exp)
 
     cdef tuple _get_labels(self):
         cdef double x
-        cdef double normal_probs[NUM_GENOTYPES]
-        cdef double tumour_probs[NUM_GENOTYPES]
-        cdef double joint_probs[NUM_JOINT_GENOTYPES] 
+        cdef double * normal_log_likelihood, * tumour_log_likelihood
+        cdef double * normal_probabilities, * tumour_probabilities
+        cdef double * joint_probabilities 
         cdef JointBinaryQualityCounterRow row
+        cdef tuple labels
         
         row = self._iter._current_row
         
-        self._get_probs(
-                        normal_probs,
-                        row._normal_data,
-                        self._mu_N, self._log_pi_N
-                        )
-        self._get_probs(
-                        tumour_probs,
-                        row._tumour_data,
-                        self._mu_T, self._log_pi_T
-                        )        
+        normal_log_likelihood = snv_mix_2_log_likelihood(row._normal_data, self._mu_N, self._qual_map, NUM_GENOTYPES)
+        tumour_log_likelihood = snv_mix_2_log_likelihood(row._tumour_data, self._mu_T, self._qual_map, NUM_GENOTYPES)
         
-        self._combine_probs(joint_probs, normal_probs, tumour_probs)
+        normal_probabilities = get_mixture_posterior(normal_log_likelihood, self._log_pi_N, NUM_GENOTYPES)
+        tumour_probabilities = get_mixture_posterior(tumour_log_likelihood, self._log_pi_T, NUM_GENOTYPES)
         
-        return tuple([x for x in joint_probs[:NUM_JOINT_GENOTYPES]])
-                                
-    cdef void _get_probs(self,
-                         double probs[NUM_GENOTYPES],
-                         base_map_qualities_struct data,
-                         double mu[NUM_GENOTYPES],
-                         double log_pi[NUM_GENOTYPES]):
-        '''
-        Return posterior probabilities under SNVMix2 model.
-        '''
-        cdef int i, genotype
-        cdef double p, q, r, m, aligned_wrong, aligned_right
+        joint_probabilities = combine_independent_probs(normal_probabilities,
+                                                        tumour_probabilities,
+                                                        NUM_GENOTYPES,
+                                                        NUM_GENOTYPES)
         
-        for genotype in range(NUM_GENOTYPES):
-            p = 0            
-            m = mu[genotype]
-            
-            for read_index in range(data.depth.A):
-                q = self._qual_map[data.base_quals.A[read_index]]
-                r = self._qual_map[data.map_quals.A[read_index]]
-                
-                p += self._compute_single_base_log_prob(q, r, m)
-            
-            for read_index in range(data.depth.B):
-                q = (1 - self._qual_map[data.base_quals.B[read_index]]) / 3
-                r = self._qual_map[data.map_quals.B[read_index]]
-                
-                p += self._compute_single_base_log_prob(q, r, m)
-            
-            probs[genotype] = log_pi[genotype] + p 
+        labels = tuple([x for x in joint_probabilities[:NUM_JOINT_GENOTYPES]])
         
-        log_space_normalise_row(probs, NUM_GENOTYPES)
+        # Cleanup allocated arrays.
+        free(normal_log_likelihood)
+        free(tumour_log_likelihood)
+        free(normal_probabilities)
+        free(tumour_probabilities)
+        free(joint_probabilities)
         
-        for i in range(NUM_GENOTYPES):
-            probs[i] = exp(probs[i]) 
-            
-    cdef double _compute_single_base_log_prob(self, double q, double r, double m):
-        aligned_wrong = 0.5 * (1 - r)
-        aligned_right = r * ((1 - q) * (1 - m) + q * m)
-        
-        return log(aligned_wrong + aligned_right)
-    
-    cdef void _combine_probs(self,
-                             double joint_probs[NUM_JOINT_GENOTYPES],
-                             double normal_probs[NUM_GENOTYPES],
-                             double tumour_probs[NUM_GENOTYPES]):
-        cdef int i, j, k
-        cdef double total
-        
-        total = 0
-        for i in range(NUM_GENOTYPES):
-            for j in range(NUM_GENOTYPES):
-                k = NUM_GENOTYPES * i + j
-                
-                joint_probs[k] = normal_probs[i] * tumour_probs[j]
-
-                total += joint_probs[k]
-                
-        for i in range(NUM_JOINT_GENOTYPES):
-            joint_probs[i] = joint_probs[i] / total         
+        return labels
         
