6f4e53ce7abfbf2f15f9b5b97ee585908691f8f5
aroth <aroth@localhost>
2011-08-11 10:46:07 -0700

    Added minimum variant depth to independent fisher method.

--- joint_snv_mix/classifiers/independent_fisher.pyx
+++ joint_snv_mix/classifiers/independent_fisher.pyx
@@ -17,6 +17,7 @@ cdef class IndependentFisherClassifierRefIterator(ClassifierRefIterator):
         self._hom_var_freq = kwargs.get('hom_var_freq', 0.9)
         self._p_value_threshold = kwargs.get('p_value_threshold', 0.05)
         self._expected_error_rate = kwargs.get('expected_error_rate', 0.001)
+        self._min_var_depth = kwargs.get('min_var_depth', 4)
 
     cdef tuple _get_labels(self):
         cdef int normal_genotype, tumour_genotype, joint_genotype 
@@ -40,8 +41,8 @@ cdef class IndependentFisherClassifierRefIterator(ClassifierRefIterator):
         '''
         Given count data call genotype. 0 - No variant, 1 - Het variant, 2 - Homozygous variant.
         '''
-        cdef int d
-        cdef float pv, var_freq
+        cdef int d, genotype
+        cdef float pv, var_freq, 
         
         d = a + b
         
@@ -52,18 +53,24 @@ cdef class IndependentFisherClassifierRefIterator(ClassifierRefIterator):
         else:
             var_freq = 0
         
-        # Counts differ from expectation due to random error and depth of coverage is acceptable.
+        # By default assume AA genotype.
+        genotype = 0
+        
+        # Check if counts differ from expectation due to random error and depth of coverage is acceptable.
         if pv <= self._p_value_threshold:
-            # Homozygous variant.
+            # Homozygous BB variant.
             if var_freq >= self._hom_var_freq:
-                return 2
-            # Heterozygous variant.
+                genotype = 2
+            # Heterozygous AB variant.
             elif var_freq >= self._min_var_freq:
-                return 1
+                genotype = 1
+        
+        # Final check to make sure there are enough variant reads otherwise call AA.
+        if b < self._min_var_depth:
+            genotype = 0
         
         # Something failed so call non-variant.        
-        
-        return 0
+        return genotype
         
     cdef float _get_significance(self, int a, int b):
         cdef int expected_a, expected_b, d
