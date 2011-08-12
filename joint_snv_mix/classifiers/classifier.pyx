399921c2242bc1ea156e7709c3435a39419e90f6
aroth <aroth@localhost>
2011-08-12 12:03:27 -0700

    Simplified classifier interface.

--- joint_snv_mix/classifiers/classifier.pyx
+++ joint_snv_mix/classifiers/classifier.pyx
@@ -1,94 +1,23 @@
 '''
-Define interfaces for Classifier, ClassifierRefIterator and ClassifierRow objects.
-
-Work with convention that all C level access of position is 0-based and Python level is 1-based.
+Define interfaces for Classifier and ClassifierRow objects.
 
 Created on 2011-07-27
 
 @author: Andrew Roth
 '''
 cdef class Classifier(object):
-    '''
-    Base clase for all objects for classifying somatics from Bam files.
-    
-    Serves two functions. 
-    1) Keeps track of available references accessible by refs property. 
-    2) Provides iterators over references via iter_ref() method.
-    '''
-    def __init__(self, Counter counter):        
-        self._counter = counter            
-        self._refs = counter.refs
-            
-    def iter_ref(self, ref, **kwargs):
-        '''
-        Returns a ClassifierIter subclass over ref.
-        '''
-        raise NotImplemented
-    
-    property refs:
-        '''
-        Read only access to list of available references.
-        '''
-        def __get__(self):
-            return self._refs
-
-cdef class ClassifierRefIterator(object):
-    '''
-    Base class for all iterator objects over a reference. Should return a ClassifierRow subclass object on each
-    iteration.
-    '''
-    def __init__(self, char * ref, JointRefIterator iter, **kwargs):
-        self._ref = ref        
-        self._iter = iter
+    def classify(self, row):
+        return self._classify(row)
         
-        self._min_normal_depth = kwargs.get('min_normal_depth', 10)
-        self._min_tumour_depth = kwargs.get('min_tumour_depth', 10)
+    cdef ClassifierRow _classify(self, PairedSampleBinomialCounterRow row):
+        cdef tuple labels
+        
+        labels = self._get_labels(row)
+        
+        return makeClassifierRow(row, labels)
     
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        '''
-        Python level next() method.
-        '''
-        self.cnext()
-        
-        return self._current_row
-    
-    cdef cnext(self):
-        '''
-        C level next method.
-        
-        All sub-classes need to re-implement the _get_labels() method for this to work.
-        '''
-        cdef int normal_depth, tumour_depth
-        cdef PairedSampleBinomialCounterRow counter_row
-        cdef tuple labels     
-        
-        while True:
-            self._iter.cnext()
-            
-            counter_row = self._iter._current_row
-            
-            normal_depth = counter_row._normal_depth
-            tumour_depth = counter_row._tumour_depth
-            
-            if normal_depth >= self._min_normal_depth and tumour_depth >= self._min_tumour_depth:
-                break
-        
-        labels = self._get_labels()
-        
-        self._current_row = makeClassifierRow(counter_row, labels)
-    
-    cdef tuple _get_labels(self):
+    cdef tuple _get_labels(self, PairedSampleBinomialCounterRow row):
         pass
-    
-    property ref:
-        '''
-        Read only access to reference which the iterator runs over.
-        '''
-        def __get__(self):
-            return self._ref
 
 cdef class ClassifierRow(object):
     '''
