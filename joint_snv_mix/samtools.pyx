0cdc7d624a6e56137be53f0a6906fb941ddc211d
AndrewJLRoth <AndrewJLRoth@localhost>
2012-01-17 09:18:23 -0800

    Most of pieces in place for samtools wrapper. Need imports still.

--- joint_snv_mix/samtools.pyx
+++ joint_snv_mix/samtools.pyx
@@ -1,29 +1,73 @@
 '''
+A simple wrapper around a core set of samtools functionality. Based on code from the pysam library. Heavily modified, to
+simplify access for JointSNVMix.
+
 Created on 2012-01-17
 
 @author: Andrew Roth
 '''
+#=======================================================================================================================
+# Fasta file
+#=======================================================================================================================
+cdef class Fastafile:
+    cdef char * _file_name
+    cdef faidx_t * _fasta_file
+
+    def __cinit__(self, char * file_name):
+        self._file_name = strdup(file_name)
+        
+        self._fasta_file = fai_load(file_name)
+
+        if self._fasta_file == NULL:
+            raise Exception("Could not open FASTA file {0}".format(file_name))
+        
+        index_file_name = file_name + "fai"
+        
+        if not os.path.exists(index_file_name):
+            raise Exception("Index not found for FASTA file {0}".format(file_name))
+
+    def close(self):
+        if self._fasta_file != NULL:
+            fai_destroy(self._fasta_file)            
+            self._fasta_file = NULL
+
+    def __dealloc__(self):
+        self.close()
+        
+        if self._file_name != NULL: 
+            free(self._file_name)
+    
+    cdef faidx_t * get_file_pointer(self):
+        return self._fasta_file
+
+    cdef char * get_reference_base(self, char * reference, int position):
+        cdef char * ref_base
+        cdef int len
+        
+        len = 1
+        
+        ref_base = self._fetch(reference, int position, int position + 1, & len)
+        
+        ref_base[0] = < char > toupper(< int > ref_base[0])
+
+        return ref_base    
+
+    cdef char * _fetch(self, char * reference, int start, int end, int * length):
+        return faidx_fetch_seq(self._fasta_file, reference, start, end - 1, length)
+
+#=======================================================================================================================
+# BAM file class
+#=======================================================================================================================
 cdef class BamFile:
+    '''
+    Heavily simplified version of pysam Samfile class. Tailored specifically for extracting data from BAM files for
+    JointSNVMix.
+    '''
     cdef bam_index_t * _index
 
     def __cinit__(self, char * file_name):
         self._file_name = file_name
         
-        self._open_file()
-        self._load_index_file()
-    
-    def __dealloc__(self):
-        bam_index_destroy(self._index)
-
-    cdef _load_index_file(self, file_name):
-        index_file_name = self._file_name + ".bai"
-        
-        if not os.path.exists(index_file_name):
-            raise Exception("Index not found for BAM file {0}".format(file_name))
-        
-        self._index = bam_index_load(file_name)
-    
-    cdef _open_file(self):
         self._bam_file = samopen(filename, mode, NULL)
         
         if self._bam_file == NULL:
@@ -31,27 +75,28 @@ cdef class BamFile:
 
         if self._bam_file.header == NULL:
             raise Exception("File {0} does not have valid header - is it SAM/BAM format?".format(self._file_name))
+
+        index_file_name = self._file_name + ".bai"
+        
+        if not os.path.exists(index_file_name):
+            raise Exception("Index not found for BAM file {0}".format(file_name))
+        
+        self._index = bam_index_load(file_name)
+            
+    def __dealloc__(self):
+        bam_index_destroy(self._index)
     
-    cdef  samfile_t * get_file_pointer(self):
+    cdef samfile_t * get_file_pointer(self):
         return self._bam_file
         
-    def pileup(self, reference=None, start=None, end=None, **kwargs):
-        cdef int rtid, rstart, rend, has_coord
-        cdef bam_plbuf_t * buf
+    def pileup(self, reference, start=None, end=None):
+        cdef int region_tid, region_start, region_end
 
-        has_coord, rtid, rstart, rend = self._parseRegion(reference, start, end, region)
+        region_tid, region_start, region_end = self._parseRegion(reference, start, end)
 
+        return IteratorColumnRegion(self, tid=rtid, start=rstart, end=rend)
 
-        if has_coord:
-            return IteratorColumnRegion(self,
-                                         tid=rtid,
-                                         start=rstart,
-                                         end=rend,
-                                         **kwargs)
-        else:
-            return IteratorColumnAllRefs(self, **kwargs)
-        
-    def _parseRegion(self, reference=None, start=None, end=None, region=None):
+    def _parseRegion(self, reference=None, start=None, end=None):
         '''
         Parse region information.
 
@@ -62,10 +107,6 @@ cdef class BamFile:
 
         Note that regions are 1-based, while start,end are python coordinates.
         '''
-        # This method's main objective is to translate from a reference to a tid. 
-        # For now, it calls bam_parse_region, which is clumsy. Might be worth
-        # implementing it all in pysam (makes use of khash).
-        
         cdef int rtid
         cdef long long rstart
         cdef long long rend
@@ -78,86 +119,36 @@ cdef class BamFile:
             try:
                 rstart = start
             except OverflowError:
-                raise ValueError('start out of range (%i)' % start)
+                raise Exception('BamFile.pileup : Start out of numerical range {0}'.format(start))
             
         if end != None: 
             try:
                 rend = end
             except OverflowError:
-                raise ValueError('end out of range (%i)' % end)
+                raise Excpetion('BamFile.pileup : End out of numerical range {0}'.format(end))
 
-        if region:
-            parts = re.split("[:-]", region)
-            reference = parts[0]
-            if len(parts) >= 2: rstart = int(parts[1]) - 1
-            if len(parts) >= 3: rend = int(parts[2])
+        rtid = reference_to_tid(self._bam_file.header, reference)
+        
+        if rtid < 0: 
+            raise Exception('BamFile.pileup : Invalid reference {0}.'.format(reference))
+        
+        if rstart > rend: 
+            raise Exception('BamFile.pileup : Invalid coordinates: start {0} > end {1}.'.format(rstart, rend))
+        
+        if not 0 <= rstart < max_pos: 
+            raise Exception('BamFile.pileup : Start out of range {0}'.format(rstart))
+        
+        if not 0 <= rend <= max_pos: 
+            raise ValueError('BamFile.pileup : End out of range {0}'.format(rend))
 
-        if not reference: return 0, 0, 0, 0
+        return rtid, rstart, rend
 
-        rtid = self.gettid(reference)
-        if rtid < 0: raise ValueError("invalid reference `%s`" % reference)
-        if rstart > rend: raise ValueError('invalid coordinates: start (%i) > end (%i)' % (rstart, rend))
-        if not 0 <= rstart < max_pos: raise ValueError('start out of range (%i)' % rstart)
-        if not 0 <= rend <= max_pos: raise ValueError('end out of range (%i)' % rend)
-
-        return 1, rtid, rstart, rend
-
+#=======================================================================================================================
+# Aligned Read
+#=======================================================================================================================
 cdef class AlignedRead:
     def __dealloc__(self):
         bam_destroy1(self._delegate)
-
-    def overlap(self, uint32_t start, uint32_t end):
-        """return number of aligned bases of read overlapping the interval *start* and *end*
-        on the reference sequence.
-        """
-        cdef uint32_t k, i, pos, overlap
-        cdef int op, o
-        cdef uint32_t * cigar_p
-        cdef bam1_t * src 
-
-        overlap = 0
-
-        src = self._delegate
-        if src.core.n_cigar == 0: return 0
-        pos = src.core.pos
-        o = 0
-
-        cigar_p = bam1_cigar(src)
-        for k from 0 <= k < src.core.n_cigar:
-            op = cigar_p[k] & BAM_CIGAR_MASK
-            l = cigar_p[k] >> BAM_CIGAR_SHIFT
-
-            if op == BAM_CMATCH:
-                o = min(pos + l, end) - max(pos, start)
-                if o > 0: overlap += o
-
-            if op == BAM_CMATCH or op == BAM_CDEL or op == BAM_CREF_SKIP:
-                pos += l
-
-        return overlap
-
-    def opt(self, tag):
-        """retrieves optional data given a two-letter *tag*"""
-        #see bam_aux.c: bam_aux_get() and bam_aux2i() etc 
-        cdef uint8_t * v
-        v = bam_aux_get(self._delegate, tag)
-        if v == NULL: raise KeyError("tag '%s' not present" % tag)
-        type = chr(v[0])
-        if type == 'c' or type == 'C' or type == 's' or type == 'S':
-            return < int > bam_aux2i(v)            
-        elif type == 'i' or type == 'I':
-            return < int32_t > bam_aux2i(v)            
-        elif type == 'f' or type == 'F':
-            return < float > bam_aux2f(v)
-        elif type == 'd' or type == 'D':
-            return < double > bam_aux2d(v)
-        elif type == 'A':
-            # there might a more efficient way
-            # to convert a char into a string
-            return '%c' % < char > bam_aux2A(v)
-        elif type == 'Z':
-            return < char *> bam_aux2Z(v)
-
 
 cdef makeAlignedRead(bam1_t * src):
     cdef AlignedRead dest = AlignedRead.__new__(AlignedRead)
@@ -166,6 +157,9 @@ cdef makeAlignedRead(bam1_t * src):
     
     return dest
 
+#=======================================================================================================================
+# Iterators
+#=======================================================================================================================
 cdef class IteratorRowRegion(object):
     cdef int _return_value    
     
@@ -208,28 +202,6 @@ cdef class IteratorRowRegion(object):
             raise StopIteration
         
         return makeAlignedRead(self._bam_struct)
-
-cdef class IterData(object):
-    cdef char * _seq
-    
-    cdef int _seq_len
-    cdef int _tid
-    
-    cdef samfile_t * _bam_file_ptr
-    
-    cdef faidx_t * _fasta_file_ptr
-    
-    cdef bam_iter_t _iter
-
-    def __cinit__(self, samfile_t * bam_file_ptr, faidx_t * fasta_file_ptr, bam_iter_t iter):        
-        self._bam_file_ptr = bam_file_ptr
-        
-        self._fasta_file_ptr = fasta_file_ptr
-        
-        self._iter = iter
-        
-        self._seq = NULL
-        self._tid = -1
 
 cdef class IteratorColumn:
     cdef int _tid
@@ -318,6 +290,55 @@ cdef class IteratorColumn:
         self._pileup_iter = bam_plp_init(& __advance_all, & self._iter_data)
     
         bam_plp_set_mask(self._pileup_iter, self._mask)
+
+#=======================================================================================================================
+# Pileup objects
+#=======================================================================================================================
+cdef class PileupProxy:
+    cdef bam_pileup1_t * _plp
+    cdef int _tid
+    cdef int _pos
+    cdef int _n_pu
+   
+cdef class PileupRead:
+    '''
+    A read aligned to a column.
+    '''
+    cdef int _indel
+    cdef int _level
+    
+    cdef int32_t  _qpos
+   
+    cdef uint32_t _is_del
+    cdef uint32_t _is_head
+    cdef uint32_t _is_tail
+    
+    cdef AlignedRead _alignment
+
+# Factory methods
+#---------------------------------------------------------------------------------------------------------------------- 
+cdef makePileupProxy(bam_pileup1_t * plp, int tid, int pos, int n):
+    cdef PileupProxy dest = PileupProxy.__new__(PileupProxy)
+    
+    dest._plp = plp
+    dest._tid = tid
+    dest._pos = pos
+    dest._n = n
+    
+    return dest
+
+cdef makePileupRead(bam_pileup1_t * src):
+    cdef PileupRead dest = PileupRead.__new__(PileupRead)
+    
+    dest._alignment = makeAlignedRead(src.b)
+    dest._qpos = src.qpos
+    dest._indel = src.indel
+    dest._level = src.level
+    dest._is_del = src.is_del
+    dest._is_head = src.is_head
+    dest._is_tail = src.is_tail
+    
+    return dest
         
 #=======================================================================================================================
 # Utility functions and structs
@@ -343,3 +364,18 @@ cdef int __advance_all(void * data, bam1_t * b):
     d = < __iterdata *> data
     
     return bam_iter_read(d.bam_file_ptr.x.bam, d.iter, b)
+
+cdef int reference_to_tid(bam_header_t * header, char * s)
+    cdef khiter_t iter
+    cdef khash_t(s) * h
+    
+    bam_init_header_hash(header)
+    
+    h = < khash_t(s) *> header.hash
+    
+    iter = kh_get(s, h, s) 
+    
+    if iter == kh_end(h):
+        return -1
+    
+    return kh_value(h, iter)
