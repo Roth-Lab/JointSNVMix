389bcfab11265dcf6346fae592460ceb2a574ac5
aroth <aroth@localhost>
2012-01-17 17:53:47 -0800

    Initial cleanup of pileup code.

--- joint_snv_mix/samtools/pileup.pxd
+++ joint_snv_mix/samtools/pileup.pxd
@@ -4,65 +4,70 @@ Created on 2012-01-17
 @author: Andrew Roth
 '''
 from libc.stdint cimport int32_t, uint8_t, uint32_t, uint64_t
-from libc.stdlib cimport free
+from libc.stdlib cimport free, malloc
+from libc.string cimport strdup
 
-from joint_snv_mix.samtools.samtools_clib import bam_destroy1, bam_iter_read, bam_plp_reset, bam_plp_init, \
+from joint_snv_mix.samtools.samtools_clib cimport bam_destroy1, bam_iter_read, bam_plp_reset, bam_plp_init, \
                                                  bam_plp_destroy, bam_plp_auto, bam_plp_set_mask, BAM_DEF_MASK, \
                                                  bam_dup1, bam_iter_t, bam1_t, samfile_t, const_bam_pileup1_t_ptr, \
-                                                 bam_plp_t, bam_pileup1_t
+                                                 bam_plp_t, bam_pileup1_t, bam_iter_query, bam1_seq, bam1_qual
 
-cdef class AlignedRead:
-    cdef  bam1_t * _delegate
+from joint_snv_mix.samtools.bam cimport BamFile
+                                                 
+ctypedef struct iter_data_t:
+    int tid
+    samfile_t * bam_file_ptr
+    bam_iter_t iter
+    int seq_len
+    char * seq
 
-cdef class IteratorRowRegion(object):
-    cdef int _return_value    
-    
-    cdef bam_iter_t _iter
-    
-    cdef bam1_t * _bam_struct
-    cdef samfile_t * _bam_file_ptr
-    
-    cdef BamFile _bam_file
+#cdef class AlignedRead:
+#    cdef  bam1_t * _delegate
 
-    cdef bam1_t * getCurrent(self)
-    cdef cnext(self)
-
-cdef class IteratorColumnRegion:
+cdef class PileupRegionIterator:
     cdef int _tid
     cdef int _pos
     cdef int _n_plp
     cdef int _mask
     
     cdef BamFile _bam_file
-#    cdef FastaFile _fasta_file
     
     cdef const_bam_pileup1_t_ptr _plp    
     cdef bam_plp_t _pileup_iter    
-    cdef __iter_data _iter_data 
-    
-    cdef IteratorRowRegion _iter
+
+    cdef iter_data_t _iter_data 
 
     cdef cnext(self)
     cdef _setup_iterator_data(self, int tid, int start, int end)
     
-cdef class PileupProxy:
-    cdef bam_pileup1_t * _plp
+#cdef class PileupProxy:
+#    cdef int _num_reads
+#    cdef bam_pileup1_t * _pileup_ptr
+#    
+#    cdef int _tid
+#    cdef int _pos
+    
+cdef class PileupColumn:
     cdef int _tid
     cdef int _pos
-    cdef int _n_pu
-   
-cdef class PileupRead:
-    cdef int _indel
-    cdef int _level
     
-    cdef int32_t  _qpos
-   
-    cdef uint32_t _is_del
-    cdef uint32_t _is_head
-    cdef uint32_t _is_tail
+    cdef char * _bases
     
-    cdef AlignedRead _alignment
-
-cdef makeAlignedRead(bam1_t * src)
-cdef makePileupProxy(bam_pileup1_t * plp, int tid, int pos, int n)
-cdef makePileupRead(bam_pileup1_t * src)
+    cdef int * _base_quals
+    cdef int * _map_quals
+    
+#cdef class PileupRead:
+#    cdef int _indel_size
+#    cdef int _level_in_viewer
+#    
+#    cdef int32_t  _query_pos
+#   
+#    cdef uint32_t _is_del
+#    cdef uint32_t _is_head
+#    cdef uint32_t _is_tail
+#    
+#    cdef AlignedRead _alignment
+#
+#cdef makeAlignedRead(bam1_t * src)
+#cdef makePileupProxy(bam_pileup1_t * plp, int tid, int pos, int n)
+#cdef makePileupRead(bam_pileup1_t * src)
--- joint_snv_mix/samtools/pileup.pyx
+++ joint_snv_mix/samtools/pileup.pyx
@@ -3,59 +3,26 @@ Created on 2012-01-17
 
 @author: Andrew Roth
 '''
-cdef class AlignedRead:
-    def __dealloc__(self):
-        bam_destroy1(self._delegate)
+# Global constants
+cdef int max_pos = 2 << 29
+cdef char * bam_nt16_rev_table = "=ACMGRSVTWYHKDBN"
 
-cdef class IteratorRowRegion(object):
-    def __cinit__(self, BamFile bam_file, int tid, int start, int end):        
-        # Makes sure that samfile stays alive as long as the iterator is alive.
+#cdef class AlignedRead:
+#    def __dealloc__(self):
+#        bam_destroy1(self._delegate)
+
+cdef class PileupRegionIterator:
+    def __cinit__(self, BamFile bam_file, int tid=0, int start=0, int stop=max_pos):        
         self._bam_file = bam_file
-
-        self._bam_file_ptr = self._bam_file.get_file_pointer()
-
-        self._return_value = 0
-
-        self._iter = bam_iter_query(self._bam_file.get_index(), tid, start, end)
-
-        self._bam_struct = bam_init1()
-        
-    def __dealloc__(self):
-        bam_destroy1(self._bam_struct)        
-
-    def __iter__(self):
-        return self 
-
-    cdef bam1_t * getCurrent(self):
-        return self._bam_struct
-
-    cdef cnext(self):
-        self._return_value = bam_iter_read(self._bam_file_ptr.x.bam,
-                                           self._iter,
-                                           self._bam_struct)
-        
-    def __next__(self): 
-        self.cnext()
-
-        if self.retval < 0: 
-            raise StopIteration
-        
-        return makeAlignedRead(self._bam_struct)
-
-cdef class IteratorColumnRegion:
-    def __cinit__(self, BamFile bam_file, int tid=0, int start=0, int end=max_pos):        
-        self._bam_file = bam_file
-        
-#        self._fasta_file = fasta_file
-                                
+                            
         self._mask = BAM_DEF_MASK
 
-        self._tid = 0
+        self._tid = tid
         self._pos = 0
         self._n_plp = 0
         self._plp = NULL
                 
-        self._setup_iterator_data(self._tid, start, end)
+        self._setup_iterator_data(self._tid, start, stop)
 
     def __dealloc__(self):
         # reset in order to avoid memory leak messages for iterators that have
@@ -68,8 +35,7 @@ cdef class IteratorColumnRegion:
             self._pileup_iter = < bam_plp_t > NULL
     
         if self._iter_data.seq != NULL: 
-            free(self._iter_data.seq)
-            
+            free(self._iter_data.seq)            
             self._iter_data.seq = NULL
         
     def __iter__(self):
@@ -85,10 +51,11 @@ cdef class IteratorColumnRegion:
             if self._plp == NULL:
                 raise StopIteration
             
-            return makePileupProxy(< bam_pileup1_t *> self._plp,
-                                     self._tid,
-                                     self._pos,
-                                     self._n_plp)    
+            return makePileupColumn(< bam_pileup1_t *> self._plp,
+                                    self._tid,
+                                    self._pos,
+                                    self._n_plp)
+#            return self._tid, self._pos, self._n_plp
     
     cdef cnext(self):
         '''
@@ -102,73 +69,174 @@ cdef class IteratorColumnRegion:
                                  & self._pos,
                                  & self._n_plp)
         
-    cdef _setup_iterator_data(self, int tid, int start, int end):
+    cdef _setup_iterator_data(self, int tid, int start, int stop):
         '''
         Setup the iterator structure.
-        '''    
-        self._iter = IteratorRowRegion(self._bam_file, tid, start, end)
+        '''
+        cdef bam_iter_t iter
+        
+        iter = bam_iter_query(self._bam_file.get_index(), tid, start, stop)
         
         self._iter_data.bam_file_ptr = self._bam_file.get_file_pointer()
-        self._iter_data.fasta_file_ptr = NULL
-        self._iter_data.iter = self._iter._iter
+        self._iter_data.iter = iter
         self._iter_data.seq = NULL
         self._iter_data.tid = -1
             
-        self._pileup_iter = bam_plp_init(& __advance_all, & self._iter_data)
+        self._pileup_iter = bam_plp_init(& advance_all, & self._iter_data)
     
         bam_plp_set_mask(self._pileup_iter, self._mask)
+        
+        print self._iter_data.tid
 
-#=======================================================================================================================
-# Pileup objects
-#=======================================================================================================================
-cdef class PileupProxy:
-    pass
+#cdef class PileupProxy:
+#    pass
 
-cdef class PileupRead:
-    pass
+cdef class PileupColumn:
+    def __dealloc__(self):
+        free(self._bases)
+        free(self._base_quals)
+        free(self._map_quals)
+    
+    def __str__(self):
+        return self._tid, self._pos
+
+#cdef class PileupRead:
+#    pass
 
 #=======================================================================================================================
 # Factory methods
 #=======================================================================================================================
-cdef makeAlignedRead(bam1_t * src):
-    cdef AlignedRead dest = AlignedRead.__new__(AlignedRead)
-    
-    dest._delegate = bam_dup1(src)
-    
-    return dest
+#cdef makeAlignedRead(bam1_t * src):
+#    cdef AlignedRead dest = AlignedRead.__new__(AlignedRead)
+#    
+#    dest._delegate = bam_dup1(src)
+#    
+#    return dest
  
-cdef makePileupProxy(bam_pileup1_t * plp, int tid, int pos, int n):
-    cdef PileupProxy dest = PileupProxy.__new__(PileupProxy)
-    
-    dest._plp = plp
-    dest._tid = tid
-    dest._pos = pos
-    dest._n_pu = n
-    
-    return dest
+#cdef makePileupProxy(bam_pileup1_t * plp, int tid, int pos, int n):
+#    cdef PileupProxy dest = PileupProxy.__new__(PileupProxy)
+#    
+#    dest._num_reads = n
+#    dest._pileup_ptr = plp
+#    
+#    dest._tid = tid
+#    dest._pos = pos
+#
+#    return dest
 
-cdef makePileupRead(bam_pileup1_t * src):
-    cdef PileupRead dest = PileupRead.__new__(PileupRead)
+#cdef makePileupRead(bam_pileup1_t * src):
+#    cdef PileupRead dest = PileupRead.__new__(PileupRead)
+#    
+#    dest._alignment = makeAlignedRead(src.b)
+#    
+#    dest._query_pos = src.qpos
+#    
+#    dest._indel_size = src.indel
+#
+#    dest._level_in_viewer = src.level
+#
+#    dest._is_del = src.is_del
+#    dest._is_head = src.is_head
+#    dest._is_tail = src.is_tail
+#    
+#    return dest
+
+cdef makePileupColumn(bam_pileup1_t * plp, int tid, int pos, int n):
+    cdef int i, depth, index, qpos
+    cdef bam1_t * alignment
+    cdef bam_pileup1_t * pileup
+
+    cdef PileupColumn column = PileupColumn.__new__(PileupColumn)
+                    
+    column._tid = tid
+    column._pos = pos
     
-    dest._alignment = makeAlignedRead(src.b)
-    dest._qpos = src.qpos
-    dest._indel = src.indel
-    dest._level = src.level
-    dest._is_del = src.is_del
-    dest._is_head = src.is_head
-    dest._is_tail = src.is_tail
+    # Find out how many non deletion reads there are.
+    depth = get_covered_depth(plp, n)    
     
-    return dest
+    column._depth = depth
+     
+    column._bases = < char *> malloc(sizeof(char) * depth)
+    column._base_quals = < int *> malloc(sizeof(int) * depth)
+    column._map_quals = < int *> malloc(sizeof(int) * depth)  
+        
+    index = 0
+
+    for i in range(n):
+        pileup = & plp[i]
+        
+        if pileup.is_del:
+            continue
+        
+        qpos = pileup.qpos
+        
+        alignment = bam_dup1(pileup.b)
+        
+        column._bases[index] = get_base(alignment, qpos)            
+
+        column._base_quals[index] = get_qual(alignment, qpos)
+        
+        column._map_quals[index] = alignment.core.qual
+        
+        bam_destroy1(alignment)
+        
+        index += 1
+    
+    return column
         
 #=======================================================================================================================
 # Utility functions and structs
 #=======================================================================================================================
-cdef int __advance_all(void * data, bam1_t * b):
+cdef int advance_all(void * data, bam1_t * b):
     '''
     Advance without any read filtering.
     '''
-    cdef __iter_data * d
+    cdef iter_data_t * d
     
-    d = < __iter_data *> data
+    d = < iter_data_t *> data
     
     return bam_iter_read(d.bam_file_ptr.x.bam, d.iter, b)
+
+cdef char get_base(bam1_t * src, int pos):
+    cdef uint8_t * p
+    cdef char base
+
+    if src.core.l_qseq == 0: 
+        return None
+    
+    if not src.core.l_qseq:
+        return None
+
+    seq = bam1_seq(src)
+    
+    base = bam_nt16_rev_table[seq[pos / 2] >> 4 * (1 - pos % 2) & 0xf]
+    
+    return base
+
+cdef int get_qual(bam1_t * src, int pos):    
+    cdef uint8_t * p
+
+    p = bam1_qual(src)
+    if p[0] == 0xff:
+        return None
+
+    return p[pos]
+
+cdef int get_covered_depth(bam_pileup1_t * plp, int n):
+    '''
+    Gets the number of reads without deletions in a proxy object.
+    '''
+    cdef int i, depth
+    cdef bam_pileup1_t * pileup
+    
+    depth = 0
+    
+    for i in range(n):
+        pileup = & plp[i]
+        
+        if pileup.is_del:
+            continue
+        
+        depth += 1
+    
+    return depth
