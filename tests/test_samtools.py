7d6e2072132c040490e8b93f86b8b580aada867f
AndrewJLRoth <AndrewJLRoth@localhost>
2012-01-18 18:53:01 -0800

    Renamed unittest file for samtools functionality.

---
+++ tests/test_samtools.py
@@ -0,0 +1,44 @@
+'''
+Created on 2012-01-17
+
+@author: andrew
+'''
+import unittest
+
+from joint_snv_mix.samtools.bam import BamFile
+
+
+class Test(unittest.TestCase):
+    def test_references(self):
+        bam = BamFile('data/multiple_chromosome.bam')
+        
+        self.assertSetEqual(set(('1', '2')), set(bam.references))
+    
+    def test_invalid_reference(self):
+        bam = BamFile('data/multiple_chromosome.bam')
+        
+        self.assertRaises(Exception, bam.get_pileup_iterator, 'X')
+        
+    def test_pileup(self):
+        bam = BamFile('data/pileup.bam')
+        
+        pileup_iter = bam.get_pileup_iterator('1')
+        
+        pileup_column = pileup_iter.next()
+        self.assertEqual(pileup_column.position, 33)      
+        self.assertListEqual(pileup_column.base_quals, [0])
+        self.assertListEqual(pileup_column.map_quals, [10])
+        
+        pileup_column = pileup_iter.next()
+        self.assertEqual(pileup_column.position, 34)        
+        self.assertListEqual(pileup_column.base_quals, [0, 30])
+        self.assertListEqual(pileup_column.map_quals, [10, 20])
+
+        pileup_column = pileup_iter.next()
+        self.assertEqual(pileup_column.position, 35)      
+        self.assertListEqual(pileup_column.base_quals, [0, 30, 93])
+        self.assertListEqual(pileup_column.map_quals, [10, 20, 100])
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
