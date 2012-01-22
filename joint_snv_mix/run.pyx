b2b544108597a6ff29adb14cd0ceace306ead035
AndrewJLRoth <AndrewJLRoth@localhost>
2012-01-22 13:22:07 -0800

    Re-wrote run script in Cython for speed.

--- joint_snv_mix/run.pyx
+++ joint_snv_mix/run.pyx
@@ -10,6 +10,8 @@ from joint_snv_mix.counter import JointBinaryCounter
 from joint_snv_mix.models.joint_snv_mix import JointSnvMixModel, JointSnvMixPriors, JointSnvMixParameters
 from joint_snv_mix.samtools import BamFile, FastaFile
 
+from joint_snv_mix.counter cimport JointBinaryCounterRow, JointBinaryData
+
 #=======================================================================================================================
 # Functions for running classification.
 #=======================================================================================================================
@@ -22,77 +24,37 @@ def classify(args):
     
     classify_data_set(counter, model, args)
 
-def classify_data_set(counter, classifier, args):
-    writer = ResultsWriter(file_name=args.out_file)
+def classify_data_set(counter, JointSnvMixModel model, args):
+    cdef int tumour_var_counts
+    cdef bint print_all
     
-    if args.chromosome is not None:
-        positions_iter = counter.get_ref_iterator(args.chromosome)
+    cdef JointBinaryCounterRow row
+    cdef JointBinaryData data
+    
+    print_all = args.print_all_positions
+    
+    writer = CResultsWriter(file_name=args.out_file)
+    
+    if args.chromosome is None:
+        refs = counter.refs
     else:
-        positions_iter = get_genome_iterator(counter)
+        refs = [args.chromosome, ]
+        
+    for ref in refs:
+        positions_iter = counter.get_iterator(ref=ref)
+        
+        for row in positions_iter:
+            data = row._data
+            tumour_var_counts = row._data._b_T
+            
+            if not print_all and tumour_var_counts == 0:
+                continue
+            
+            model._predict(data)
+            
+            writer.write_position(row, model._resp)
     
-    for row in positions_iter:
-        if not args.print_all_positions and row.tumour_var_counts == 0:
-            continue 
-        
-        probs = classifier.predict(row.data)
-        
-        writer.write_position(row, probs)
-
-class ResultsWriter(object):
-    info = [
-            'chrom',
-            'position',
-            'ref_base',
-            'var_base',
-            'normal_counts_a',
-            'normal_counts_b',
-            'tumour_counts_a',
-            'tumour_counts_b'
-            ]
-    
-    probs = [
-             'p_AA_AA',
-             'p_AA_AB',
-             'p_AA_BB',
-             'p_AB_AA',
-             'p_AB_AB',
-             'p_AB_BB',
-             'p_BB_AA',
-             'p_BB_AB',
-             'p_BB_BB'                            
-             ]
-    
-    fields = info + probs
-    
-    def __init__(self, file_name=None):
-        if file_name is None:
-            fh = sys.stdout
-        else:
-            fh = open(file_name, 'w')
-        
-        self._writer = csv.DictWriter(fh, ResultsWriter.fields, delimiter='\t')
-        
-        self._writer.writeheader()
-        
-    def write_position(self, row, probs):
-        out_row = {}
-        out_row['chrom'] = row.ref
-        out_row['position'] = row.position
-        out_row['ref_base'] = row.ref_base
-        out_row['var_base'] = row.var_base
-        
-        counts = row.counts
-        
-        out_row['normal_counts_a'] = counts[0]
-        out_row['normal_counts_b'] = counts[1]
-        out_row['tumour_counts_a'] = counts[2]
-        out_row['tumour_counts_b'] = counts[3]
-        
-        probs = dict(zip(ResultsWriter.probs, probs))
-        
-        out_row = dict(out_row.items() + probs.items())
-        
-        self._writer.writerow(out_row)
+    writer.close()
          
 #=======================================================================================================================
 # Functions for training a model.
@@ -111,37 +73,48 @@ def train(args):
     model.params.write_to_file(args.estimated_parameters_file)        
 
 def create_training_data_set(counter, args):
-    i = 0
+    cdef int i, d_N, d_T, min_d_N, min_d_T, max_d_N, max_d_T, skip_size
+    
+    cdef JointBinaryCounterRow row
+    cdef JointBinaryData data
     
     data_set = []
     
-    if args.chromosome is not None:
-        positions_iter = counter.get_ref_iterator(args.chromosome)
-    else:
-        positions_iter = get_genome_iterator(counter)
+    i = 0
     
-    for row in positions_iter:
-        if args.min_normal_depth <= row.normal_depth <= args.max_normal_depth and \
-           args.min_tumour_depth <= row.tumour_depth <= args.max_tumour_depth:
-            i += 1
+    min_d_N = args.min_normal_depth
+    min_d_T = args.min_tumour_depth
+    
+    max_d_N = args.max_normal_depth
+    max_d_T = args.max_tumour_depth
+    
+    skip_size = args.skip_size
+    
+    if args.chromosome is None:
+        refs = counter.refs
+    else:
+        refs = [args.chromosome, ]
+    
+    for ref in sorted(refs):
+        positions_iter = counter.get_iterator(ref=ref)
+        
+        for row in positions_iter:
+            data = row._data
             
-            if i % args.skip_size == 0:
-                data_set.append(row.data)
+            d_N = data._a_N + data._b_N
+            d_T = data._a_T + data._b_T
+            
+            if  (min_d_N <= d_N <= max_d_N) and (min_d_T <= d_T <= max_d_T):
+                i += 1
+                
+                if i % skip_size == 0:
+                    data_set.append(data)
     
     return data_set
 
 #=======================================================================================================================
 # Helper factory classes for setting up parsers and classifiers.
 #=======================================================================================================================
-def get_genome_iterator(counter):
-    refs = sorted(counter.refs)
-    
-    for ref in refs:
-        ref_iter = counter.get_ref_iterator(ref)
-        
-        for row in ref_iter:
-            yield row
-
 class CounterFactory(object):
     def get_counter(self, args):
         if args.model == 'jsm1':
