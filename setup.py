12b354f3169a9bf8b9cfcae7f1faa73665efd504
aroth <aroth@localhost>
2012-02-24 12:14:50 -0800

    Updated setup to include rf file.

--- setup.py
+++ setup.py
@@ -75,14 +75,35 @@ for file_name in glob.glob('joint_snv_mix/models/*.c'):
     
     models.append(ext)
 
+#=======================================================================================================================
+# Post Processing
+#=======================================================================================================================
+post_processing_include = ['joint_snv_mix', 'joint_snv_mix/post_processing', 'include/samtools']
+
+post_processing = []
+
+for file_name in glob.glob('joint_snv_mix/post_processing/*.c'):
+    base_name = os.path.basename(file_name)
+    root, ext = os.path.splitext(base_name)
+    
+    module = "joint_snv_mix.post_processing.{0}".format(root)
+    
+    ext = Extension(module, [file_name, ], include_dirs=models_include)
+    
+    post_processing.append(ext)
+
+#=======================================================================================================================
+# Main setup
+#=======================================================================================================================
 ext_modules = []
 ext_modules.extend(core)
 ext_modules.extend(models)
 ext_modules.extend(samtools)
+ext_modules.extend(post_processing)
 
 setup(
       name='JointSNVMix',
-      version='0.8',
+      version='0.8-b2',
       description='A collection of tools for calling somatic mutations in paired normal/tumour data.',
       author='Andrew Roth',
       author_email='andrewjlroth@gmail.com',
@@ -91,8 +112,10 @@ setup(
       packages=[ 
                 'joint_snv_mix',
                 'joint_snv_mix.samtools',
-                'joint_snv_mix.models'
+                'joint_snv_mix.models',
+                'joint_snv_mix.post_processing'
                 ],
+      package_data={'joint_snv_mix.post_processing': ['tn_excap.rf.bz2', ]},
       ext_modules=ext_modules,
       scripts=['jsm.py']
      )
